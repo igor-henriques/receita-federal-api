252aebc204d31a88e46d4ba2ab8a5d9c904f92c8
Igor Henriques <lycanium@hotmail.com>
2022-04-08 21:58:09 -0300

    console poc removed

--- situacao-cpf/GlobalUsing.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System.Diagnostics;
-global using OpenQA.Selenium.Chrome;
-global using situacao_cpf;
-global using OpenQA.Selenium;
-global using OpenQA.Selenium.Support.UI;
-global using situacao_cpf.Interfaces;
-global using situacao_cpf.Repositories;
